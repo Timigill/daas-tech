d2719566b72883f9a2751266df72de87dec9e30c
hussainsheikh110 <140153529+hussainsheikh110@users.noreply.github.com>
2025-06-24 11:28:18 +0500

    Responsive

--- src/app/components/trust.js
+++ src/app/components/trust.js
@@ -1,46 +1,31 @@
-import React from 'react'
+import React from 'react';
 
-function trust() {
+
+function Trust() {
   return (
-    <div className="d-flex flex-column align-items-center text-center">
-      <p
-        className="text-white-50"
-        style={{
-          fontFamily: "Inter, sans-serif",
-          fontSize: "1rem",
-          maxWidth: 500,
-          margin: "0 auto",
-        }}
-      >
+    <div className="d-flex flex-column align-items-center text-center trust-section">
+      <p className="trust-text text-white-50">
         Over 50+ business trust us
       </p>
 
       <div className="review-slider mt-4">
-  <div className="slider-fade-left"></div>
-  <div className="slider-fade-right"></div>
-  <div className="slider-track">
-    <div className="slider-content">
-      {[...Array(2)].map((_, i) => (
-        <React.Fragment key={i}>
-          <img src="/google-reviews.png" alt="Review" className="slider-image" />
-          <img src="/google-reviews.png" alt="Review" className="slider-image" />
-          <img src="/google-reviews.png" alt="Review" className="slider-image" />
-          <img src="/google-reviews.png" alt="Review" className="slider-image" />
-        </React.Fragment>
-      ))}
+        <div className="slider-fade-left"></div>
+        <div className="slider-fade-right"></div>
+        <div className="slider-track">
+          <div className="slider-content">
+            {[...Array(2)].map((_, i) => (
+              <React.Fragment key={i}>
+                <img src="/google-reviews.png" alt="Review" className="slider-image" />
+                <img src="/google-reviews.png" alt="Review" className="slider-image" />
+                <img src="/google-reviews.png" alt="Review" className="slider-image" />
+                <img src="/google-reviews.png" alt="Review" className="slider-image" />
+              </React.Fragment>
+            ))}
+          </div>
+        </div>
+      </div>
     </div>
-  </div>
-</div>
-
-
-
-    </div>
-
-
-
-
-
-  )
+  );
 }
 
-export default trust
+export default Trust;
