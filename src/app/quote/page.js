f43510f3584248f7e58e2a536a2d46c606eca415
hussainsheikh110 <140153529+hussainsheikh110@users.noreply.github.com>
2025-06-24 13:32:01 +0500

    update-hero-section

--- src/app/quote/page.js
+++ src/app/quote/page.js
@@ -86,7 +86,61 @@ export default function Page() {
     };
 
     return (
+        
         <div className="mt-5 px-3 px-md-5">
+            <div className="d-flex flex-column align-items-center text-center">
+      <div>
+        {/* Tagline */}
+        <span
+          className="badge px-3 py-2"
+          
+             style={{
+          padding: "4px 10px",
+          fontSize: 12,
+          color: "#fff",
+          // background: "#1e1e23",
+          border: "1px solid rgb(17 17 17)",
+          borderRadius: 8,
+          width: "fit-content",
+          fontWeight: 500,
+          marginBottom: 12,
+        }}
+         
+        >
+          Quote Page
+        </span>
+
+        {/* Main Heading */}
+        <h1
+          className="mt-1"
+          style={{
+            fontFamily: "Inter, sans-serif",
+            fontWeight: 600,
+            // fontSize: "3.5rem",
+            lineHeight: 1.1,
+            color: "#ffffff",
+            maxWidth: 700,
+            margin: "0 auto",
+          }}
+        >
+          Precision Quotes for Premium Projects
+        </h1>
+
+        {/* Subheading */}
+        <p
+          className="mt-3 text-white-50"
+          style={{
+            fontFamily: "Inter, sans-serif",
+            fontSize: "1rem",
+            maxWidth: 500,
+            margin: "0 auto",
+          }}
+        >
+          No Guesswork, No Hidden Cost.Just Clear,Customized Pricing-in Minutes
+        </p>
+      </div>
+    </div>
+            
             <form onSubmit={handleSubmit} noValidate>
                 {/* Main Form Section */}
                 <div className="row justify-content-center mt-3 g-5">
