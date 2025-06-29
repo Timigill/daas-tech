0746d25f329d97908af11ee0d168835bbaabf682
Timigill <taimoorgill311@gmail.com>
2025-06-29 22:36:48 +0500

    Updated Image Optimization

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,17 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+/** @type {import('next').Neconst nextConfig = {};
 
-export default nextConfig;
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    images: {
+      remotePatterns: [
+        {
+          protocol: 'https',
+          hostname: 'res.cloudinary.com',
+        },
+      ],
+    },
+  };
+  
+  export default nextConfig;
+  
