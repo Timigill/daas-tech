10a6fe9f18070b6d91efece9e082f6e1708b3b91
Timigill <taimoorgill311@gmail.com>
2025-07-03 17:44:58 +0500

    base url

--- src/app/blog/[id]/page.js
+++ src/app/blog/[id]/page.js
@@ -1,7 +1,8 @@
 import BlogDetailClient from "./BlogDetailsClient";
 
 async function getBlog(id) {
-  const res = await fetch(`/api/blog/${id}`, {
+  const baseUrl = process.env.BASE_URL || "http://localhost:3000";
+  const res = await fetch(`${baseUrl}/api/blog/${id}`, {
     cache: "no-store",
   });
   if (!res.ok) throw new Error("Failed to fetch blog");
