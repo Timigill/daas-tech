73ffe737f998214f526920ce6bc9fc62f4fc965b
Hamza Rajpoot <hamzashahbaz9121@gmail.com>
2025-07-08 22:57:46 +0500

    Modal

--- src/app/components/QuotelistModel.js
+++ src/app/components/QuotelistModel.js
@@ -5,18 +5,23 @@ import { Modal, Button } from 'react-bootstrap';
 export default function ContactModal({ show, onHide, quote }) {
   return (
     <Modal show={show} onHide={onHide} centered>
-      <Modal.Header  closeButton style={{borderBottom: "none", background: "linear-gradient(to bottom right, #5b2a86, #270082)", color: "#fff" }}>
-        <Modal.Title>Quote Detail</Modal.Title>
-      </Modal.Header>
-      <Modal.Body  style={{ background: "linear-gradient(to bottom right, #5b2a86, #270082)", color: "#fff" }}>
+    
+    
+      <Modal.Body  className='position-relative' style={{ background: "linear-gradient(to bottom right, #5b2a86, #270082)", color: "#fff" }}>
+        
+        <Modal.Title className='my-3 fw-bold'>Quote Detail
+     
+        </Modal.Title>
         <p><strong>Name:</strong> {quote?.contactName}</p>
         <p><strong>Email:</strong> {quote?.email}</p>
         <p><strong>Phone:</strong> {quote?.phone}</p>
-    <p><strong>Project Types:</strong></p>
-<ul style={{ paddingLeft: "1.2rem" }}>
-  {(Array.isArray(quote.projectType) 
-    ? quote.projectType 
-    : quote.projectType?.split(",") || []
+  <p><strong>Project Types:</strong></p>
+<ul style={{ paddingLeft: "1.5rem", marginBottom: "1rem" }}>
+  {(typeof quote?.projectType === 'string'
+    ? quote.projectType.split(',')
+    : Array.isArray(quote?.projectType)
+    ? quote.projectType
+    : []
   ).map((type, idx) => (
     <li key={idx} style={{ marginBottom: "4px" }}>
       {type.trim()}
@@ -24,14 +29,24 @@ export default function ContactModal({ show, onHide, quote }) {
   ))}
 </ul>
 
+
+
+
+
+    
+        <p><strong>Company:</strong> {quote?.company || '-'}</p>
+      
         <p><strong>Created At:</strong> {new Date(quote?.createdAt).toLocaleDateString()}</p>
         <p><strong>Budget:</strong> {quote?.budget || '-'}</p>  
+   <p><strong>Status:</strong> {quote?.status}</p>
         <p><strong>Timeline:</strong> {quote?.timeline || '-'}</p>
         {/* Add more quote details */}
+
+        <Button variant="light" className='position-absolute bottom-0 end-0 mx-3 my-3' onClick={onHide}>Close</Button>
+
       </Modal.Body>
-      <Modal.Footer style={{borderTop: "none" ,background: "linear-gradient(to bottom right, #5b2a86, #270082)", color: "#fff" }}>
-        <Button variant="light" onClick={onHide}>Close</Button>
-      </Modal.Footer>
+      
+      
     </Modal>
   );
 }
