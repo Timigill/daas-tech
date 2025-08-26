3d4b0cf5cdc73e0d8a81203be5e1e69b9e133569
RimCodePixie <Rimshatabassum0@gmail.com>
2025-08-26 08:33:38 +0500

    Google Analytics

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,6 +2,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import { Inter } from 'next/font/google';
 import HeaderFooterWrapper from './components/HeaderFooterWrapper'; // new client component
 import './globals.css';
+import Script from 'next/script';
 
 // import '@/app/custom-bootstrap.scss';
 const inter = Inter({ subsets: ['latin'] });
@@ -18,18 +19,30 @@ export default function RootLayout({ children }) {
     <html lang="en" className={inter.className}>
       <head>
         <script type="text/javascript" src="//widget.trustpilot.com/bootstrap/v5/tp.widget.bootstrap.min.js" async></script>
-        <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap" rel="stylesheet" />
+       <Script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-34H5QSZZ7F"
+        />
+        <Script id="google-analytics">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-34H5QSZZ7F');
+          `}
+        </Script>
 
+        <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap" rel="stylesheet" />
         <meta name="theme-color" content="#181622" />
 
-      
+
       </head>
       <body className="text-white">
         <HeaderFooterWrapper>
-        <SpeedInsights/>
-        <ToastContainer />
-        
-        {children}
+          <SpeedInsights />
+          <ToastContainer />
+
+          {children}
         </HeaderFooterWrapper>
       </body>
     </html>
